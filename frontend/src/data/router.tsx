bff5f4991523d27265d00648c4c81a8f2f91ed01
dev.trycia <dev.trycia@gmail.com>
2020-10-04 20:51:22 -0400

    Auto stash before merge of "production" and "origin/production"

--- frontend/src/data/router.tsx
+++ frontend/src/data/router.tsx
@@ -24,7 +24,11 @@ export class Router extends React.Component<Props, State> {
                 <section className='bloc-page'>
                     <nav className='nav'>
                         <ul className='flex-grid'>
+<<<<<<< Updated upstream
                             <Link className='logo' to={`/`}><img src='../../img/logo.png' alt='logo' /></Link>
+=======
+                            <Link className='logo' to={'/'}><img src='../../img/logo.png' alt='logo' /></Link>
+>>>>>>> Stashed changes
                             <li><Link to='/'>Accueil</Link></li>
                             <li><Link to='/equipe'>Équipe</Link></li>
                             <li><Link to='/services'>Services</Link></li>
